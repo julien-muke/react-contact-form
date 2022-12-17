59cef85c71157e68aebbbc488de3618e226dd957
julien-muke <julien.muke@gmail.com>
2022-12-17 21:44:18 +0200

    Setup the Node.js server

---
+++ src/node server.js
@@ -0,0 +1,20 @@
+router.post("/contact", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message; 
+    const mail = {
+      from: name,
+      to: "***************@gmail.com",
+      subject: "Contact Form Submission",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json({ status: "ERROR" });
+      } else {
+        res.json({ status: "Message Sent" });
+      }
+    });
+  });
